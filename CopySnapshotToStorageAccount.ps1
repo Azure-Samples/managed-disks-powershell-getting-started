ffbbf2cbc43ea55aaa5a16d723071dd9b1158ffd
ramankumarlive <ramankum@microsoft.com>
2017-03-22 18:01:02 -0700

    Added readme and one PS script

---
+++ CopySnapshotToStorageAccount.ps1
@@ -0,0 +1,58 @@
+<#
+
+.DESCRIPTION
+
+This sample demonstrate how to generate SAS URI for a managed snapshot and use it to copy the snapshot to a storage account
+
+
+.NOTES
+
+1. Before you use this sample, please install the latest version of Azure PowerShell from here: http://go.microsoft.com/?linkid=9811175&clcid=0x409
+2. Provide the appropriate values for each variable. Note: The angled brackets should not be included in the values you provide.
+
+#>
+
+#Provide the subscription Id where snapshot is created
+$subscriptionId = "<Your SubscriptionId>"
+
+#Provide the name of your resource group where snapshot is created
+$resourceGroupName ="<Your Resource Group Name>"
+
+#Provide the snapshot name 
+$snapshotName = "<You snapshot name>"
+
+#Provide Shared Access Signature (SAS) expiry duration in seconds e.g. 3600.
+#Know more about SAS here: https://docs.microsoft.com/en-us/azure/storage/storage-dotnet-shared-access-signature-part-1
+$sasExpiryDuration = "<SAS Expiry Duration>"
+
+#Provide storage account name where you want to copy the snapshot. 
+$storageAccountName = "<Storage Account Name>"
+
+#Name of the storage container where the downloaded snapshot will be stored
+$storageContainerName = "<Storage Container Name>"
+
+#Provide the key of the storage account where you want to copy snapshot. 
+$storageAccountKey = 'Storage account key'
+
+#Provide the name of the VHD file to which snapshot will be copied.
+$destinationVHDFileName = "<VHD file name>"
+
+
+# You will be promopted to enter the email address and password associated with your account. Azure will authenticate and saves the credential information, and then close the window. 
+Login-AzureRmAccount
+
+
+# Set the context to the subscription Id where Snapshot is created
+Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+
+#Get the snapshot using name and resource group
+$snapshot = Get-AzureRmSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $SnapshotName 
+
+#Generate the SAS for the snapshot 
+$sas = Grant-AzureRmSnapshotAccess -ResourceGroupName $ResourceGroupName -SnapshotName $SnapshotName  -DurationInSecond $sasExpiryDuration -Access Read 
+ 
+#Create the context for the storage account which will be used to copy snapshot to the storage account 
+$destinationContext = New-AzureStorageContext –StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey  
+
+#Copy the snapshot to the storage account 
+Start-AzureStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $storageContainerName -DestContext $destinationContext -DestBlob $destinationVHDFileName
--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# Contributing
+---
+services: storage,virtual machines 
+platforms: powershell
+author: ramankum
+---
 
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+## Introduction 
+These samples demonstrate how to get started with Azure PowerShell to perform various operations Azure Managed Disks. We have listed down common scenarios for which you want to perform these operations.
+
+## Prerequisites
+1. Install the latest version of [Azure PowerShell](http://go.microsoft.com/?linkid=9811175&clcid=0x409)
+2. A Microsoft Azure subscription. If you don't have a Microsoft Azure subscription you can get a FREE trial account [here](http://go.microsoft.com/fwlink/?LinkId=330212)
+
+## Scenarios 
+
++ Create a VM by attaching an existing Managed Disks
++ Create a VM from a managed Snapshots
++ Copy a snapshot to a storage account
+
+## Running samples
+
+1. Start Windows PowerShell Integrated Scripting Environment (ISE) in **Administrator Mode**
+2. At this point, you can either:
+    - Create a new script and copy and paste a script from this repo into the new file: Click File > New
+    - Or download/clone this repo and open a script from this repo: Click File > Open
+3. Provide the appropriate values for each variable (e.g. "Subscription Name", "Storage Account Name", etc.). Note: The angled brackets should not be included in the values you provide.
+4. Run the script by pressing F5 or clicking the "Run Script" icon.
+
+## More information
+- [Azure Managed Disks overview](https://docs.microsoft.com/en-us/azure/storage/storage-managed-disks-overview)
+- [Migrate Azure VMs to Azure Managed Disks](https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-windows-migrate-to-managed-disks)
+- [Migrate from AWS and other platforms to Managed Disks](https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-windows-on-prem-to-azure)
+
