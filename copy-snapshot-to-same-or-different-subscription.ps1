d1df422ddef85f19c29291766c567d81ca20e284
Micah <32313503+MicahMcKittrick-MSFT@users.noreply.github.com>
2019-08-02 10:19:11 -0700

    microsoftdocs/azure-docs#34967

--- copy-snapshot-to-same-or-different-subscription.ps1
+++ copy-snapshot-to-same-or-different-subscription.ps1
@@ -20,11 +20,5 @@ $targetSubscriptionId='6492b1f7-f219-446b-b509-314e17e1efb0'
 #Name of the resource group where snapshot will be copied to
 $targetResourceGroupName='myTargetResourceGroupName'
 
-#Set the context to the subscription Id where snapshot will be copied to
-#If snapshot is copied to the same subscription then you can skip this step
-Select-AzureRmSubscription -SubscriptionId $targetSubscriptionId
-
-$snapshotConfig = New-AzureRmSnapshotConfig -SourceResourceId $snapshot.Id -Location $snapshot.Location -CreateOption Copy 
-
 #Create a new snapshot in the target subscription and resource group
-New-AzureRmSnapshot -Snapshot $snapshotConfig -SnapshotName $snapshotName -ResourceGroupName $targetResourceGroupName
+New-AzureRmSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $targetResourceGroupName
